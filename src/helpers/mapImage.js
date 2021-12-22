845382b9fef454f898a8c122550870f65e1e60df
j0y <12984600+j0y@users.noreply.github.com>
2021-12-22 14:19:33 +0700

    add depot, iron_express, karkand maps

--- src/helpers/mapImage.js
+++ src/helpers/mapImage.js
@@ -13,6 +13,12 @@ function mapImage(map) {
             return prefix + 'de_dust.jpg'
         case 'crossbow':
             return prefix + 'crossbow.jpg'
+        case 'depot_coop':
+            return prefix + 'depot.jpg'
+        case 'iron_express':
+            return prefix + 'iron_express.jpg'
+        case 'karkand_redux_p2':
+            return prefix + 'karkand.jpg'
         default:
             return prefix + 'default.png'
     }
