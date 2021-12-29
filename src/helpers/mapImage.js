238bfd85f2560d02aebfb2c0cca6200786162182
j0y <12984600+j0y@users.noreply.github.com>
2021-12-30 05:50:13 +0700

    add different map names

--- src/helpers/mapImage.js
+++ src/helpers/mapImage.js
@@ -5,6 +5,8 @@ function mapImage(map) {
     switch (map) {
         case 'ministry_coop':
             return prefix + 'ministry.jpg'
+        case 'ministry_open_coop':
+            return prefix + 'ministry.jpg'
         case 'italy_coop':
             return prefix + 'italy.jpg'
         case 'siege_coop':
@@ -20,6 +22,8 @@ function mapImage(map) {
         case 'karkand_redux_p2':
             return prefix + 'karkand.jpg'
         case 'dry_canal_coop_v4':
+            return prefix + 'dry_canal.jpg'
+        case 'drycanal_coop':
             return prefix + 'dry_canal.jpg'
         case 'launch_control_coop_ws':
             return prefix + 'launch.jpg'
